44f01b2ce67a89c4a328931a6e19490bf5469a73
yoel fisher <yoel12344@walla.co.il>
2023-03-19 23:36:44 +0200

    target tracking indecator

---
+++ targeting_helper.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+var player
+var enemy 
+var indicator_sprite
+
+func _ready():
+	player = get_parent().owner.get_node("player")
+	enemy = get_parent()
+	indicator_sprite = $Sprite 
+	
+func _process(delta):
+	if enemy.team == 1:
+		return #we target only enemy
+	# Get the global position of the player and the target
+	var player_position = player.global_transform.origin
+	var enemy_position = enemy.global_transform.origin + enemy.velocity
+	
+	# Calculate the direction from the player to the enemy
+	var direction = enemy_position #- player_position
+	direction = direction.normalized()
+	
+	# Calculate the position of the indicator sprite
+	var indicator_position = enemy_position  + direction 
+	
+	# Set the position of the indicator sprite
+	position = get_viewport().get_camera().unproject_position(indicator_position)
+
+	# Check if the enemy node has the VisibilityNotifier node attached
+	if enemy.has_node("VisibilityNotifier") and enemy.get_node("VisibilityNotifier").is_on_screen():
+		visible = true
+	else:
+		visible = false
