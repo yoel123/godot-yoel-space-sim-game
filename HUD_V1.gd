c739076c7eba72cc423e71b1352aa840df74f357
toffee2000 <toffee2000@gmail.com>
2023-03-29 20:08:57 +0800

    Add working camera toggle button to HUD

--- HUD_V1.gd
+++ HUD_V1.gd
@@ -9,6 +9,9 @@ var value_hull : float = 0
 var value_shield : float = 0
 var value_throttle : float = 0
 
+# Setup camera button
+var button_camera_event = InputEventAction.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# Initialise dummy values (not functional yet)
@@ -24,3 +27,15 @@ func _process(delta):
 	$Data/ProgressBar_Hull.value = value_hull
 	$Data/ProgressBar_Shield.value = value_shield
 	$Data/ProgressBar_Throttle.value = value_throttle
+	
+	# If buttons are pressed, trigger event
+	# Parse generated event
+	Input.parse_input_event(button_camera_event)
+	button_camera_event.action = "camera_toggle"
+	
+	# If pressed, set event to true
+	if ($Buttons/Button_Camera.pressed == true):
+		button_camera_event.pressed = true
+	# If not pressed, set to false
+	else:
+		button_camera_event.pressed = false
